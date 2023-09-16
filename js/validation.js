b49e569ff3b98288aac93245d429c97741edb81e
bjo-droid <bjohnstad@gmail.com>
2023-09-16 23:57:50 +0200

    added red border to unvalid input fields

--- js/validation.js
+++ js/validation.js
@@ -8,29 +8,45 @@ function validation() {
     var text;
     error_message.style.padding = "10px";
 if(name.length < 5){
+    document.getElementById("name").style.border = "5px solid red";
     text = "Please Enter valid Name";
     error_message.innerHTML = text;
     return false;
+} else {
+    document.getElementById("name").style.border = "1px solid #7389f4"
 }
 if(subject.length < 15){
+    document.getElementById("subject").style.border = "5px solid red";
     text = "Please Enter More then 15 Characters";
     error_message.innerHTML = text;
     return false;
 }
+else {
+    document.getElementById("subject").style.border = "1px solid #7389f4"
+}
 if(isNaN(phone) || phone.length != 8){
+    document.getElementById("phone").style.border = "5px solid red";
     text = "Please Enter valid Phone Number 8-digits";
     error_message.innerHTML = text;
     return false;
+} else {
+    document.getElementById("phone").style.border = "1px solid #7389f4"
 }
 if(email.indexOf("@") == -1 || email.length < 6){
+    document.getElementById("email").style.border = "5px solid red";
     text = "Please Enter valid Email";
     error_message.innerHTML = text;
     return false;
+} else {
+    document.getElementById("email").style.border = "1px solid #7389f4"
 }
 if(message.length <= 25){
+    document.getElementById("message").style.border = "5px solid red";
     text = "Please Enter More Than 25 Characters";
     error_message.innerHTML = text;
     return false;
+} else {
+    document.getElementById("message").style.border = "1px solid #7389f4"
 }
 alert("Form Submitted Successfully!");
 return true;
